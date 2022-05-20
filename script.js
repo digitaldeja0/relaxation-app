fc94cdfa974981a44a19aa5b26ebc39a68c3454d
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-05-20 13:21:10 -0400

    Add JS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const container = document.querySelector(".container");
+const circleText = document.querySelector(".circleText");
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  circleText.innerHTML = "Breath In...";
+  container.className = "container grow";
+  setTimeout(() => {
+    circleText.innerHTML = "...Hold";
+    setTimeout(() => {
+      circleText.innerHTML = "Breath Out...";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
